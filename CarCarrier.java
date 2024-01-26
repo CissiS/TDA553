04c9b1771c2dc07f8fbc4794ee496008ed968fc6
andreasessen <118916152+andreasessen@users.noreply.github.com>
2024-01-26 16:52:40 +0100

    Add files via upload

---
+++ CarCarrier.java
@@ -0,0 +1,91 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class CarCarrier extends Car implements Carrier{
+    protected double platformAngle;
+    private static final double max_platformAngle = 70;
+    private static final double min_platformAngle = 0;
+    public ArrayList<Car> cars;
+
+    public CarCarrier(){
+        super(2,300, Color.cyan, "CarCarrier");
+        cars = new ArrayList<>();
+        platformAngle = max_platformAngle;
+        stopEngine();
+    }
+    @Override
+    public void move() {
+        super.move();
+        for (Car car : cars) {
+            car.position.x = position.x;
+            car.position.y = position.y;
+        }
+    }
+
+    @Override
+    public void gas(double amount) {
+        if (amount >= 0 && amount <= 1 && platformAngle == 0) {
+            incrementSpeed(amount);
+        } else {
+            throw new IllegalArgumentException("Gas input either too high or too low, or trailer angle not 0: ");
+        }
+    }
+
+    protected void incrementAngle(double amount) {
+        platformAngle = platformAngle + amount;
+    }
+
+    protected void decrementAngle(double amount) {
+        platformAngle = platformAngle - amount;
+    }
+    @Override
+    public void raiseRamp() {
+        if (platformAngle == min_platformAngle) {
+            platformAngle = max_platformAngle;
+        } else {
+            throw new IllegalArgumentException("Max angle reached: ");
+        }
+    }
+
+    @Override
+    public void lowerRamp() {
+        if (platformAngle == max_platformAngle && currentSpeed == 0) {
+            platformAngle = min_platformAngle;
+        } else {
+            throw new IllegalArgumentException("Ramp already lowered or vehicle moving: ");
+        }
+    }
+
+    @Override
+    public void loadCar(Car car) {
+        if (isCarClose(car) && platformAngle == min_platformAngle && car.modelName != "CarCarrier") {
+            cars.add(car);
+        } else {
+            throw new IllegalArgumentException("Car not in position or ramp not lowered: ");
+        }
+    }
+
+    @Override
+    public void unloadCar() {
+        if (!cars.isEmpty() && platformAngle == min_platformAngle) {
+            cars.removeLast();
+        } else {
+            throw new IllegalArgumentException("Car not on trailer or ramp not lowered:");
+        }
+    }
+
+
+    @Override
+    public double speedFactor() {
+        return getEnginePower() * 0.02;
+    }
+
+    public void incrementSpeed(double amount){
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,getEnginePower());
+    }
+    public void decrementSpeed(double amount){
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+    }
+
+}
